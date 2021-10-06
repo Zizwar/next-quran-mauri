36dddc8b105f83fee03f14d7131e344a9ceb0ee6
Zizwar <zizwar@gmail.com>
2021-10-06 04:38:36 +0000

    add file page hafsTajweed

--- pages/layouts/sections/index/footer.js
+++ pages/layouts/sections/index/footer.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import Subscribs from './subscribe';
-import {Container,Row,Col} from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 const Footer = () => (
     <Fragment>
         <Subscribs />
@@ -8,12 +8,9 @@ const Footer = () => (
             <Container>
                 <Row>
                     <Col xs="12">
-                        
-                            <div>
-                                
-                                <h6 className="copyright-text text-white">Copyright © 2021</h6>
-                            </div>
-                       
+                        <div>
+                            <h6 className="copyright-text text-white">Copyright © 2021</h6>
+                        </div>
                     </Col>
                 </Row>
             </Container>
